a639f4407eb13c092315721cadc6814bbbc18412
Roger Beecham <r.j.beecham@leeds.ac.uk>
2020-11-28 22:30:26 +0000

    initial commit

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -1,6 +1,12 @@
 # deploy commands
 
-echo "Deleting old publication"
+# git submodule add \
+#   https://github.com/gcushen/hugo-academic.git \
+#   themes/hugo-academic
+
+# add a gh-pages branch -- https://jiafulow.github.io/blog/2020/07/09/create-gh-pages-branch-in-existing-repo/
+
+echo "Deleting old public"
 rm -rf public
 mkdir public
 git worktree prune
@@ -13,13 +19,12 @@ git worktree add -B gh-pages public origin/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
-find . -name "*.html" -type f -delete
-rm -rf themes/hugo-academic
-git submodule update --recursive --remote
+# find . -name "*.html" -type f -delete
+# rm -rf themes/hugo-academic
+# git submodule update --recursive --remote
 
 echo "Generating site"
 Rscript -e "blogdown::build_site()"
-hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
